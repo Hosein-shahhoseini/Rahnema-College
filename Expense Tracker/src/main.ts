a80827b6d09ca948100c54f47c2bc0e1ee071963
Hosein Shahhoseini <hosein.shah.hoseni.mirash@gmail.com>
2024-07-25 10:39:46 +0330

    final Expense Traker

--- Expense Tracker/src/main.ts
+++
@@ -1,6 +0,0 @@
-import {app} from "./api";
-
-const PORT = 3000;
-app.listen(PORT , () => {
-    console.log("Listening on Prot " + PORT);
-});
--- Expense Tracker/src/modules/group/create-group.ts
+++
@@ -1,9 +0,0 @@
-import { Group } from "./dto/group.dto";
-import { groups } from './../../routes/group.routes';
-
-export const createGroup = (dto : Group , groups: Group[]) => {
-    const myGroup = groups.find((x) => x.group_id = dto.group_id);
-    if(myGroup === undefined){
-        groups.push(dto);
-    }
-};
